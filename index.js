c2bf23986a41637a4cde660c860be24cdd7b7d7e
AbdullahAbdulHannan <abdullahabdulhannan.ab@gmail.com>
2023-11-25 09:24:06 +0500

    edited

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const routes=require('./Routes/Postroutes')
 const authRoutes=require('./Routes/authRoutes')
 const cors =require('cors')
-const PORT=process.env.PORT |5000
+const PORT=process.env.PORT ||5000
 app.use(express.json())
 app.use(cors())
 mongoose.connect(process.env.MONGOO_URI)
@@ -13,9 +13,9 @@ mongoose.connect(process.env.MONGOO_URI)
 .catch((err)=>console.log(err))
 app.use('/api',routes)
 app.use('/auth',authRoutes)
-// app.get('/',(req,res)=>{
-//     res.send("HI")
-// })
+app.get('/',(req,res)=>{
+    res.send("HI")
+})
 app.listen(PORT,()=>{
     console.log(PORT+ " is running fine!");
 })
