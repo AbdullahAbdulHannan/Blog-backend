06927a1f42fdcd75b23358ce9051b6e142156690
AbdullahAbdulHannan <abdullahabdulhannan.ab@gmail.com>
2023-11-25 09:13:09 +0500

    uploaded

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -3,7 +3,7 @@ const Signup = require('../controllers/Signup')
 const login=require("../controllers/login")
 const auth=require("../controllers/auth")
 const router=express.Router()
-router.post('/signup',Signup)
+router.post('/Signup',Signup)
 router.post('/login',login)
 router.post('/auth',auth)
 module.exports=router
